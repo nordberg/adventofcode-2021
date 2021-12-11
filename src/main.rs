6fb3a289c6d2c853a1ae642e76e69412ecefc6c6
Marcus Nordberg <nordbergaren93@gmail.com>
2021-12-11 14:10:31 +0100

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 mod days;
+mod utils;
 
 #[macro_use]
 extern crate strum_macros;
 
 use std::cmp::{max, min};
-use std::collections::{BTreeMap, HashMap, VecDeque};
-use std::fmt::Binary;
-use std::fs::File;
+use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
+use std::fmt::{Binary, Debug, Formatter, Write};
+use std::fs::{File, read};
 use std::io::{self, prelude::*, BufReader};
 use std::ops::Index;
+use std::usize;
+use crate::utils::get_adjacent;
 
+
+struct Thing {
+    chars: Vec<char>,
+    has_error: bool,
+}
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
 
-
     for line in reader.lines() {
-        let read_line = line.expect("Error reading the line");
+        let l = line.expect("Could not read line");
     }
 
     Ok(())
