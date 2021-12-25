a351715b49eafb15984bc3c05fa49f87535e10d0
Marcus Nordberg <nordbergaren93@gmail.com>
2021-12-25 15:33:26 +0100

    Solve day 25

---
+++ src/days/day_25.rs
@@ -0,0 +1,68 @@
+use std::collections::HashMap;
+use std::process::exit;
+
+#[derive(Eq, PartialEq)]
+enum Direction {
+    Right,
+    Down
+}
+
+pub fn solve(input: &str) {
+    let mut cucumbers = HashMap::new();
+    let mut width = 0;
+    let mut height = 0;
+    input.lines().enumerate().for_each(|(i, line)| {
+        width = line.len();
+        height = i + 1;
+        line.chars().enumerate().for_each(|(j, c)| {
+            match c {
+                '>' => {
+                    cucumbers.insert((j, i), Direction::Right);
+                }
+                'v' => {
+                    cucumbers.insert((j, i), Direction::Down);
+                }
+                _ => {}
+            }
+        });
+    });
+
+    for step in 1.. {
+        let mut moving_cucumbers = 0;
+
+        let mut updates = vec![];
+
+        cucumbers.iter().filter(|(_, dir)| **dir  == Direction::Right).for_each(|((x, y), _)| {
+            if !cucumbers.contains_key(&((x + 1) % width, *y)) {
+                updates.push((*x, *y));
+                moving_cucumbers += 1;
+            }
+        });
+
+        updates.iter().for_each(|(x, y)| {
+            cucumbers.insert(((*x + 1) % width, *y), Direction::Right);
+            cucumbers.remove(&(*x, *y));
+        });
+
+        updates.clear();
+
+        cucumbers.iter().filter(|(_, dir)| **dir  == Direction::Down).for_each(|((x, y), _)| {
+            if !cucumbers.contains_key(&(*x, (y + 1) % height)) {
+                updates.push((*x, *y));
+                moving_cucumbers += 1;
+            }
+        });
+
+        updates.iter().for_each(|(x, y)| {
+            cucumbers.insert((*x, (y + 1) % height), Direction::Down);
+            cucumbers.remove(&(*x, *y));
+        });
+
+        if moving_cucumbers == 0 {
+            println!("No movement after {} steps", step);
+            break;
+        } else {
+            println!("{} cucumbers moving after {} steps", moving_cucumbers, step);
+        }
+    }
+}
